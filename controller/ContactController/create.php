c5385d0b5b2b9a5882676590f5c84563fd43101a
Jryllianz <franckgeralddumdum@gmail.com>
2020-01-09 03:05:31 +0800

    tiny modification

--- controller/ContactController/create.php
+++ controller/ContactController/create.php
@@ -2,21 +2,21 @@
 	session_start(); 
     require_once '../../config/connection.php';
     require_once '../../model/contacts.php';
-    require_once '../../model/registration.php';
+    require_once '../../model/auth.php';
 
     $con = new connection();
 	$db = $con->connect();
 
     $contact = new Contacts($db);
-    $register = new Registration($db);
+    $auth = new Authentication($db);
 
-    $register->email = $contact->email = $_POST['email'];
+    $auth->email = $contact->email = $_POST['email'];
     $contact->user_id = $_SESSION['id'];
     $contact->name = $_POST['name'];
     $contact->company = $_POST['company'];
     $contact->phone = $_POST['phone'];
 
-    $row = $register->isEmailExist();
+    $row = $auth->isEmailExist();
     
     if($row > 0){
         echo 'exist';
